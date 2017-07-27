6d65311445150b39028e8676b4dd14b8fd440a31
Toby Chadderton <30502459+TobyChadderton@users.noreply.github.com>
2017-07-27 21:30:54 +0100

    Add files via upload

---
+++ News.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class News : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+
+    int framecount = 0;
+    public WWW news;
+    public string textWhenFailed = "Failed To Load News";
+    public string URL = "http://pineapple8.co.uk/txt/test.txt";
+
+    // Update is called once per frame
+    void Update () {
+
+        UnityEngine.UI.Text uitext = GetComponent<UnityEngine.UI.Text>();
+
+        if (framecount == 0) {
+            try
+            {
+                news = new WWW(URL);
+            }
+            catch {}
+        }
+
+        if(news.isDone)
+        {
+            if (news.text == "")
+            {
+                uitext.text = textWhenFailed;
+            }
+            else
+            {
+                uitext.text = news.text;
+            }
+        }
+        framecount += 1;
+    }
+}
